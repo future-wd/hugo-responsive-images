5a014c1c8678e9504cf417af64317acebec17d87
Sean Emerson <emerson.sean.dev@gmail.com>
2022-08-31 22:26:23 -0700

    fix: in progress

--- .testSite/assets/test.js
+++ .testSite/assets/test.js
@@ -1,99 +1,108 @@
-const checkPicture = ({ pictureId, resultsId, checks }) => {
+const checkPicture = ({ divId, checks }) => {
   // results array
   const results = [];
+  // debug header
+  const debug = [];
   // get picture tag by id
-  const picture = document.getElementById(pictureId);
-  if (picture) {
+  const div = document.getElementById(divId);
+  if (div) {
     // get first img tag
-    const img = picture.getElementsByTagName('img')[0];
+    const img = div.getElementsByTagName('img')[0];
     if (img) {
-      checks['img'].forEach((value) => {
-        if (value.prop === 'class') {
-          const missingClasses = []
-          value.value.forEach ((className) => {
-            // img fluid then lazyloaded
-            classes = img.className.split(' ');
-            const found = classes.includes(className)
-            if (found === false) {
-              missingClasses.push(className)
-            }
-          })
-          if (missingClasses.length) {
-            results.push(
-              `Classes of ${value.value} <span class="text-danger"><strong>FAIL</strong></span> (${missingClasses} not found)`
-            );
-          } else {
-            results.push(
-              `Classes of ${value.value} <span class="text-success"><strong>PASS</strong></span>`
-            );
-          }
-        } else {
-          const propertyValue = img.getAttribute(value.prop);
-          if (propertyValue) {
-            if (propertyValue === value.value) {
+      const imgChecks = checks.img;
+      if (imgChecks) {
+        imgChecks.forEach((value) => {
+          if (value.prop === 'class') {
+            const missingClasses = [];
+            value.value.forEach((className) => {
+              // img fluid then lazyloaded
+              classes = img.className.split(' ');
+              const found = classes.includes(className);
+              if (found === false) {
+                missingClasses.push(className);
+              }
+            });
+            if (missingClasses.length) {
               results.push(
-                `${value.prop}="${value.value}" <span class="text-success"><strong>PASS</strong></span>`
+                `Classes of ${value.value} <span class="text-danger"><strong>FAIL</strong></span> (${missingClasses} not found)`
               );
             } else {
               results.push(
-                `${value.prop}="${value.value}" <span class="text-danger"><strong>FAIL</strong></span> (${value.prop}="${propertyValue}")`
+                `Classes of ${value.value} <span class="text-success"><strong>PASS</strong></span>`
               );
             }
           } else {
-            results.push(
-              `<span class="text-danger">Cannot find property ${value.prop}`
-            );
+            const propertyValue = img.getAttribute(value.prop);
+            if (propertyValue) {
+              if (propertyValue === value.value) {
+                results.push(
+                  `${value.prop}="${value.value}" <span class="text-success"><strong>PASS</strong></span>`
+                );
+              } else {
+                results.push(
+                  `${value.prop}="${value.value}" <span class="text-danger"><strong>FAIL</strong></span> (${value.prop}="${propertyValue}")`
+                );
+              }
+            } else {
+              results.push(
+                `<span class="text-danger">Cannot find property ${value.prop}`
+              );
+            }
           }
-        }
-      });
+        });
+      }
     } else {
-      results.push(`cannot find <img> <picture id="${pictureId}">`);
+      results.push(`cannot find <img> <picture id="${divId}">`);
     }
     // get sources
-    const sources = picture.getElementsByTagName('source');
-    checks.source.forEach((value) => {
-      // if types is passed
-      if (value.prop === 'types') {
-        if (sources) {
-          const sourceFormats = [];
-          let count = 1;
-          for (const source of sources) {
-            const typeValue = source.getAttribute('type');
-            if (typeValue) {
-              // remove 'image/' from type
-              const imageType = typeValue.slice(6);
-              sourceFormats.push(imageType);
-            } else {
-              results.push(`cannot find type property in source tag #${count}`);
+    sourceCheck = checks.source;
+    if (sourceCheck) {
+      const sources = div.getElementsByTagName('source');
+      if (sources) {
+        checks.source.forEach((value) => {
+          // if types is passed
+          if (value.prop === 'types') {
+            if (sources) {
+              const sourceFormats = [];
+              let count = 1;
+              for (const source of sources) {
+                const typeValue = source.getAttribute('type');
+                if (typeValue) {
+                  // remove 'image/' from type
+                  const imageType = typeValue.slice(6);
+                  sourceFormats.push(imageType);
+                } else {
+                  results.push(
+                    `cannot find type property in source tag #${count}`
+                  );
+                }
+                count += 1;
+              }
+              if (value.value === sourceFormats) {
+                results.push(
+                  `Source formats of ${value.value} <span class="text-success"><strong>PASS</strong></span>`
+                );
+              } else {
+                results.push(
+                  `Source formats of ${value.value} <span class="text-danger"><strong>FAIL</strong></span> formats are ${sourceFormats}`
+                );
+              }
             }
-            count += 1;
-          }
-          if (value.value === sourceFormats) {
-            results.push(
-              `Source formats of ${value.value} <span class="text-success"><strong>PASS</strong></span>`
-            );
           } else {
-            results.push(
-              `Source formats of ${value.value} <span class="text-danger"><strong>FAIL</strong></span> formats are ${sourceFormats}`
-            );
+            debug.push(' no type value in config');
           }
-        }
-      } else {
-        alert(' no type value in config');
+        });
       }
-    });
+    }
   } else {
-    results.push(`cannot find <picture id="${pictureId}">`);
+    results.push(`cannot find picture id="${divId}"`);
   }
 
-  const resultsDiv = document.getElementById(resultsId);
+  const resultsDiv = document.getElementById(`${divId}-results`);
   let resultsList = '';
   results.forEach((value) => {
     resultsList += `<li>${value}</li>`;
   });
-  // const resultsList = results.map(value => {
-  //   `<li>${value}</li>`
-  // })
 
   if (resultsDiv) {
     resultsDiv.innerHTML = `
@@ -103,18 +112,32 @@ const checkPicture = ({ pictureId, resultsId, checks }) => {
     </ul>
     `;
   } else {
-    alert('cannot find <div id="${resultsId}"');
+    debug.push(`cannot find &lt;div id="${divId}-results"&gt;`);
+  }
+
+  const debugDiv = document.getElementById('js-debug');
+
+  if (debugDiv) {
+    let debugList = '';
+    debug.forEach((value) => {
+      debugList += `<li>${value}</li>`;
+    });
+
+    debugDiv.innerHTML = `
+    <ul>
+      ${debugList}
+    </ul>
+    `;
   }
 };
 
 checkPicture({
-  pictureId: 'js-picture1',
-  resultsId: 'js-picture1-results',
+  divId: 'js-test1',
   checks: {
     img: [
-      { prop: 'sizes', value: '50vw' },
-      { prop: 'alt', value: 'test image' },
-      { prop: 'title', value: 'image title' },
+      { prop: 'data-sizes', value: 'auto' },
+      { prop: 'alt', value: 'Test Image Alt' },
+      { prop: 'title', value: 'Test Image Title' },
       { prop: 'class', value: ['img-fluid', 'lazyload'] },
     ],
     source: [{ prop: 'types', value: ['webp', 'jpeg'] }],
@@ -122,16 +145,30 @@ checkPicture({
 });
 
 checkPicture({
-  pictureId: 'js-picture3',
-  resultsId: 'js-picture3-results',
+  divId: 'js-test2',
   checks: {
-    img: [
-      { prop: 'sizes', value: '50vw' },
-      { prop: 'alt', value: 'test image' },
-      { prop: 'title', value: 'image title' },
-      { prop: 'loading', value: 'lazy'},
-      { prop: 'class', value: ['img-fluid', 'lazyload'] },
-    ],
-    source: [{ prop: 'types', value: ['webp', 'jpeg'] }],
+    img: [],
+    source: [{ prop: 'types', value: ['webp', 'png', 'jpeg'] }],
   },
 });
+checkPicture({
+  divId: 'js-test3',
+  checks: {
+    img: [{ prop: 'width', value: 400 }],
+  },
+});
+
+// checkPicture({
+//   pictureId: 'js-picture3',
+//   resultsId: 'js-picture3-results',
+//   checks: {
+//     img: [
+//       { prop: 'sizes', value: '50vw' },
+//       { prop: 'alt', value: 'test image' },
+//       { prop: 'title', value: 'image title' },
+//       { prop: 'loading', value: 'lazy'},
+//       { prop: 'class', value: ['img-fluid', 'lazyload'] },
+//     ],
+//     source: [{ prop: 'types', value: ['webp', 'jpeg'] }],
+//   },
+// });
--- .testSite/content/tests/page-resources/index.md
+++ .testSite/content/tests/page-resources/index.md
@@ -7,6 +7,26 @@ type: tests
 test_image: test.jpg
 image:
   renderHook: false
+testImages:
+- id: js-test1
+  title: 1. Basic
+  subtitle: Alt and title set
+  params:
+    alt: Test Image Alt
+    title: Test Image Title
+- id: js-test2
+  title: 2. Formats
+  subtitle: original (jpeg), png, webp
+  params:
+    alt: Formats
+    formats: [original, png, webp]
+- id: js-test3
+  title: 3. Width and densities
+  subtitle: widths 400, densities 1,2,3
+  params:
+    alt: Width, densities
+    width: 400
+    densities: [1,2,3]
 --- 
 
 <!-- Image with renderHook: false
